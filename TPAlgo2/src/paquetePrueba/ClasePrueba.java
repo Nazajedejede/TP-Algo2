f24ec051a07e4a301201e9e3974b9ce02238ad8f
luciomfernandez <luciomfernandez.ar@gmail.com>
2018-05-31 17:45:28 -0300

    Update ClasePrueba.java

--- TPAlgo2/src/paquetePrueba/ClasePrueba.java
+++ TPAlgo2/src/paquetePrueba/ClasePrueba.java
@@ -3,7 +3,9 @@ package paquetePrueba;
 public class ClasePrueba {
 
 	public static void main(String[] args) {
-		System.out.println("Prueba");
+		System.out.println("Prueba");	
+		System.out.println("Prueba2");	
+	
 	}
 
 }
